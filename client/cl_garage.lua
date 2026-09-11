586f2f029e4093e0cf77539cbbb730e71deeb1e1
BayloraiEX <117157428+BayloraiEX@users.noreply.github.com>
2026-09-10 23:41:05 -0700

    V2 Overhaul

--- client/cl_garage.lua
+++ client/cl_garage.lua
@@ -1,203 +1,77 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
------ | CREATING GARAGE MENU | -----
-lib.registerContext({
-    id = 'burgershot_garage',
-    title = 'Burgershot Garage',
-    options = {
-        {
-            title = 'Burgershot Bike',
-            description = 'Pull out Burgershots Bike',
-            icon = 'motorcycle',
-            event = 'bd-burgershot:client:spawnBike'
+local function Notify(description)
+    lib.notify({
+        id = 'burgershot_garage',
+        title = 'Burgershots Garage',
+        description = description,
+        showDuration = false,
+        position = 'top-right',
+        style = {
+            backgroundColor = '#141517',
+            color = '#F08080',
+            ['.description'] = {
+              color = '#909296'
+            }
         },
-        {
-            title = 'Burgershot Electric Bike',
-            description = 'Pull out Burgershots Bike 2',
-            icon = 'motorcycle',
-            event = 'bd-burgershot:client:spawnBike2'
-        },
-    },
-})
+        icon = 'square-parking',
+        iconColor = '#F08080'
+    })
+end
 
------ | REGISTER CONTEXT MENU TO "bd-burgershot:client:garageMenu" | -----
-RegisterNetEvent('bd-burgershot:client:garageMenu', function()
+local function OpenGarageMenu()
+    local grade = QBCore.Functions.GetPlayerData().job.grade.level
+    local options = {}
+
+    for i, veh in ipairs(Config.GarageVehicles) do
+        if grade >= (veh.minGrade or 0) then
+            options[#options + 1] = {
+                title = veh.label,
+                description = veh.description,
+                icon = veh.icon,
+                event = 'bd-burgershot:client:spawnGarageVehicle',
+                args = i,
+            }
+        end
+    end
+
+    lib.registerContext({
+        id = 'burgershot_garage',
+        title = 'Burgershot Garage',
+        options = options,
+    })
     lib.showContext('burgershot_garage')
+end
+
+RegisterNetEvent('bd-burgershot:client:garageMenu', OpenGarageMenu)
+RegisterNetEvent('bd-burgershot:client:garageMenu1', OpenGarageMenu)
+RegisterNetEvent('bd-burgershot:client:garageMenu2', OpenGarageMenu)
+
+RegisterNetEvent('bd-burgershot:client:spawnGarageVehicle', function(index)
+    local veh = Config.GarageVehicles[index]
+    if not veh then return end
+
+    QBCore.Functions.SpawnVehicle(veh.model, function(spawned)
+        SetVehicleNumberPlateText(spawned, Config.VehiclePlate)
+        DecorSetFloat(spawned, 't_vehicle', 1)
+        SetEntityAsMissionEntity(spawned, true, true)
+        TaskWarpPedIntoVehicle(PlayerPedId(), spawned, -1)
+        TriggerEvent('vehiclekeys:client:SetOwner', QBCore.Functions.GetPlate(spawned))
+        SetVehicleEngineOn(spawned, true, true)
+        SetVehicleFuelLevel(spawned, veh.fuel or 100.0)
+        SetVehicleDirtLevel(spawned, 0.0)
+    end, veh.spawn, true)
 end)
 
------ | CREATING GARAGE MENU1 | -----
-lib.registerContext({
-    id = 'burgershot_garage1',
-    title = 'Burgershot Garage',
-    options = {
-        {
-            title = 'Burgershot Van',
-            description = 'Pull out Burgershots Van',
-            icon = 'van-shuttle',
-            event = 'bd-burgershot:client:spawnVan'
-        },
-        {
-            title = 'Burgershot Bike',
-            description = 'Pull out Burgershots Bike',
-            icon = 'motorcycle',
-            event = 'bd-burgershot:client:spawnBike'
-        },
-        {
-            title = 'Burgershot Electric Bike',
-            description = 'Pull out Burgershots Bike 2',
-            icon = 'motorcycle',
-            event = 'bd-burgershot:client:spawnBike2'
-        },
-    },
-})
-
------ | REGISTER CONTEXT MENU TO "bd-burgershot:client:garageMenu1" | -----
-RegisterNetEvent('bd-burgershot:client:garageMenu1', function()
-    lib.showContext('burgershot_garage1')
-end)
-
------ | CREATING GARAGE MENU2 | -----
-lib.registerContext({
-    id = 'burgershot_garage2',
-    title = 'Burgershot Garage',
-    options = {
-        {
-            title = 'Burgershot Van',
-            description = 'Pull out Burgershots Van',
-            icon = 'van-shuttle',
-            event = 'bd-burgershot:client:spawnVan'
-        },
-        {
-            title = 'Burgershot Car',
-            description = 'Pull out Burgershots Car',
-            icon = 'car',
-            event = 'bd-burgershot:client:spawnCar'
-        },
-        {
-            title = 'Burgershot Bike',
-            description = 'Pull out Burgershots Bike',
-            icon = 'motorcycle',
-            event = 'bd-burgershot:client:spawnBike'
-        },
-        {
-            title = 'Burgershot Electric Bike',
-            description = 'Pull out Burgershots Bike 2',
-            icon = 'motorcycle',
-            event = 'bd-burgershot:client:spawnBike2'
-        },
-    },
-})
-
------ | REGISTER CONTEXT MENU TO "bd-burgershot:client:garageMenu2" | -----
-RegisterNetEvent('bd-burgershot:client:garageMenu2', function()
-    lib.showContext('burgershot_garage2')
-end)
-
------ | CREATING THE STORE LOGIC FOR VEHICLES | -----
 RegisterNetEvent('bd-burgershot:client:storeGarage', function()
     local veh = QBCore.Functions.GetClosestVehicle()
-    if DecorExistOn((veh), "t_vehicle") then
+    if DecorExistOn(veh, 't_vehicle') then
         QBCore.Functions.DeleteVehicle(veh)
-        lib.notify({
-            id = 'burgershot_garage',
-            title = 'Burgershots Garage',
-            description = 'You returned the vehicle.',
-            showDuration = false,
-            position = 'top-right',
-            style = {
-                backgroundColor = '#141517',
-                color = '#F08080',
-                ['.description'] = {
-                  color = '#909296'
-                }
-            },
-            icon = 'square-parking',
-            iconColor = '#F08080'
-        })
+        if Config.KeySystem == 'wasabi' then
+            exports.wasabi_carlock:RemoveKey(Config.VehiclePlate)
+        end
+        Notify('You returned the vehicle.')
     else
-        lib.notify({
-            id = 'burgershot_garage',
-            title = 'Burgershots Garage',
-            description = 'This is not a work vehicle.',
-            showDuration = false,
-            position = 'top-right',
-            style = {
-                backgroundColor = '#141517',
-                color = '#F08080',
-                ['.description'] = {
-                  color = '#909296'
-                }
-            },
-            icon = 'square-parking',
-            iconColor = '#F08080'
-        })
+        Notify('This is not a work vehicle.')
     end
-end)
-
------ | CREATING CAR SPAWN | -----
-RegisterNetEvent('bd-burgershot:client:spawnCar', function(carvehicle)
-    local carvehicle = Config.CarVehicle
-    local carcoords = Config.CarSpawn
-    QBCore.Functions.SpawnVehicle(carvehicle, function(carveh)
-        SetVehicleNumberPlateText(carveh, "BURGER")
-        DecorSetFloat(carveh, "t_vehicle", 1)
-        SetEntityAsMissionEntity(carveh, true, true)
-        TaskWarpPedIntoVehicle(PlayerPedId(), carveh, -1)
-        TriggerEvent("vehiclekeys:client:SetOwner", QBCore.Functions.GetPlate(carveh))
-        SetVehicleEngineOn(carveh, true, true)
-        CurrentPlate = QBCore.Functions.GetPlate(carveh)
-        SetVehicleFuelLevel(carveh, 500.0)
-        SetVehicleDirtLevel(carveh, 0)
-    end, carcoords, true)
-end)
-
------ | CREATING BIKE SPAWN | -----
-RegisterNetEvent('bd-burgershot:client:spawnBike', function(bikevehicle)
-    local bikevehicle = Config.BikeVehicle
-    local bikecoords = Config.BikeSpawn
-    QBCore.Functions.SpawnVehicle(bikevehicle, function(bikeveh)
-        SetVehicleNumberPlateText(bikevehicle, "BURGER")
-        DecorSetFloat(bikeveh, "t_vehicle", 1)
-        SetEntityAsMissionEntity(bikeveh, true, true)
-        TaskWarpPedIntoVehicle(PlayerPedId(), bikeveh, -1)
-        TriggerEvent("vehiclekeys:client:SetOwner", QBCore.Functions.GetPlate(bikeveh))
-        SetVehicleEngineOn(bikeveh, true, true)
-        CurrentPlate = QBCore.Functions.GetPlate(bikeveh)
-        SetVehicleFuelLevel(bikeveh, 275.0)
-        SetVehicleDirtLevel(bikeveh, 0)
-    end, bikecoords, true)
-end)
-
------ | CREATING BIKE2 SPAWN | -----
-RegisterNetEvent('bd-burgershot:client:spawnBike2', function(bike2vehicle)
-    local bike2vehicle = Config.Bike2Vehicle
-    local bike2coords = Config.Bike2Spawn
-    QBCore.Functions.SpawnVehicle(bike2vehicle, function(bike2veh)
-        SetVehicleNumberPlateText(bike2vehicle, "BURGER")
-        DecorSetFloat(bike2veh, "t_vehicle", 1)
-        SetEntityAsMissionEntity(bike2veh, true, true)
-        TaskWarpPedIntoVehicle(PlayerPedId(), bike2veh, -1)
-        TriggerEvent("vehiclekeys:client:SetOwner", QBCore.Functions.GetPlate(bike2veh))
-        SetVehicleEngineOn(bike2veh, true, true)
-        CurrentPlate = QBCore.Functions.GetPlate(bike2veh)
-        SetVehicleFuelLevel(bike2veh, 275.0)
-        SetVehicleDirtLevel(bike2veh, 0)
-    end, bike2coords, true)
-end)
-
------ | CREATING VAN SPAWN | -----
-RegisterNetEvent('bd-burgershot:client:spawnVan', function(vanvehicle)
-    local vanvehicle = Config.VanVehicle
-    local vancoords = Config.VanSpawn
-    QBCore.Functions.SpawnVehicle(vanvehicle, function(vanveh)
-        SetVehicleNumberPlateText(vanveh, "BURGER")
-        DecorSetFloat(vanveh, "t_vehicle", 1)
-        SetEntityAsMissionEntity(vanveh, true, true)
-        TaskWarpPedIntoVehicle(PlayerPedId(), vanveh, -1)
-        TriggerEvent("vehiclekeys:client:SetOwner", QBCore.Functions.GetPlate(vanveh))
-        SetVehicleEngineOn(vanveh, true, true)
-        CurrentPlate = QBCore.Functions.GetPlate(vanveh)
-        SetVehicleFuelLevel(vanveh, 1000.0)
-        SetVehicleDirtLevel(vanveh, 0)
-    end, vancoords, true)
 end)
